375d25d582a60f0b58e106a052e1e6b0f80216c8
KimiYaoLujian <47030233+KimiYaoLujian@users.noreply.github.com>
2021-06-29 15:26:48 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'b2507f0b2e9d4acab8a1918f625da0fb',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Kimi Blog',
+  domain: 'https://www.notion.so/Kimi-s-Blog-b2507f0b2e9d4acab8a1918f625da0fb',
+  author: 'Kimi Yao',
 
   // open graph metadata (optional)
   description: 'Example site description',
